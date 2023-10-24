94d387bdd23d5c184ab7c4aadb7ca0b5ae3a2eca
RillSvai <kirilkvas07@gmail.com>
2023-10-24 14:06:06 +0300

    Update About

--- UrlShortener.Utility/SD.cs
+++ UrlShortener.Utility/SD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Html;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,12 @@ namespace UrlShortener.Utility
     {
         public const string Role_Admin = "Admin";
         public const string Role_Customer = "Customer";
-        public static string AboutText { get; set; } 
-            = "Some about text which user with role of 'admin' can edit";
+        public static HtmlString AboutText { get; set; } 
+            = new HtmlString("<h4>1) I made simple client-side validation URL with 'Uri' and didn`t allow equal urls in my database.\n" +
+			"<br/> 2) Generating short url (with random token with custom length that user chose) for example 'https://localhost:7050/Hv3S'" +
+                "<br/> 3) Next i create RedirectorController that catch any token in url" +
+                "<br/> 4) Finnaly we checking or our database contain that token" +
+                "<br/> That`s all. We can improve short url if we set RedirectorController as default and our short url will be shorter (without word redirector)</h4>");
         public static User? User { get; set; }
     }
 }
