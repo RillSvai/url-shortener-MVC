0c60a37c028343768998d6c1d36c4afe848fff9c
RillSvai <kirilkvas07@gmail.com>
2023-10-24 14:08:01 +0300

    a bit update about

--- UrlShortener.Utility/SD.cs
+++ UrlShortener.Utility/SD.cs
@@ -14,7 +14,7 @@ namespace UrlShortener.Utility
         public const string Role_Customer = "Customer";
         public static HtmlString AboutText { get; set; } 
             = new HtmlString("<h4>1) I made simple client-side validation URL with 'Uri' and didn`t allow equal urls in my database.\n" +
-			"<br/> 2) Generating short url (with random token with custom length that user chose) for example 'https://localhost:7050/Hv3S'" +
+			"<br/> 2) Generating short url (with random token with custom length that user chose) for example 'https://localhost:7050/redirector/Hv3S'" +
                 "<br/> 3) Next i create RedirectorController that catch any token in url" +
                 "<br/> 4) Finnaly we checking or our database contain that token" +
                 "<br/> That`s all. We can improve short url if we set RedirectorController as default and our short url will be shorter (without word redirector)</h4>");
